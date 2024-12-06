135ea8204ced6bf7613e3c5380a5be3930de9a26
Nikitus200 <sm.nikita2007@mail.ru>
2024-12-07 00:33:57 +0500

    Модуль 16

---
+++ module_16/module_16_3.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+@app.get("/users")
+async def get_message() -> dict:
+    return users
+
+@app.post("/user/{username}/{age}")
+async def add_message(username: str, age: int) -> str:
+    user_id = str(int(max(users, key=int)) + 1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_message(user_id: str, username: str, age: int) -> str:
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"The user {user_id} is updated"
+
+@app.delete("/user/{user_id}")
+async def delete_message(user_id: str):
+    users.pop(user_id)
+    return f"The user {user_id} is deleted"
+
+
